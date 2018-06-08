2158383bb6c2d1844d9d516115a9b70af2a0c34f
karlajagan <karla_jagannathan@hotmail.com>
2018-06-07 23:41:50 -0400

    inserting and deleting from a table

--- main.py
+++ main.py
@@ -20,12 +20,25 @@ tasks = []
 
 @app.route('/', methods=['POST','GET'])
 def index():
-
     if request.method == 'POST':
-        task = request.form['task']
-        tasks.append(task)
+        task_name = request.form['task']
+        name_task = Task(task_name)
+        db.session.add(name_task)
+        db.session.commit()
 
+    tasks = Task.query.all()
     return render_template('todos.html', title='Get it Done!', tasks=tasks)
+
+@app.route('/delete-task', methods=['POST'])
+def delete_task():
+
+    task_id = int(request.form['task-id'])
+    task = Task.query.get(task_id)
+    db.session.delete(task)
+    db.session.commit()
+
+    return redirect('/')
+
 
 if __name__ == '__main__':
     app.run()
